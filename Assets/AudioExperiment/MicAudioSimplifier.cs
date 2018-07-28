0f27ace9eb9a3ddc5a87106a956af32ec170a3b8
Jeanclyde Cruz <ignis14311@gmail.com>
2018-07-27 21:04:46 -1000

    7.27 checkpoint

--- Assets/AudioExperiment/MicAudioSimplifier.cs
+++ Assets/AudioExperiment/MicAudioSimplifier.cs
@@ -1,25 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioMeasureCS))]
 public class MicAudioSimplifier : MonoBehaviour {
     public Hashtable pitchRange;
-    public float PickupThreshold;
-    private AudioMeasureCS audioMeasureCSInstance;
+    public float PickupThreshold;//threshold for picking up pitch
+    public float PitchTolerance;
+    private AudioMeasureCS AudioMeasureCSInstance;
+
+
+    float GetAveragePitch() {
+        float Average = 0;
+
+        
 
 
 
 
+        return Average;
+
+
+    }
+
+
+    List<float> AudioSampleHistory;
+    int AudioSampleHistoryLimit;//used to keep list from overgrowing
+    float durationToAnalyze = 0.5f;//how far back in time to analyze
+    float totalDeltaTime=0;
+    void RecordAudioSamples() {
+        float currentPitch = AudioMeasureCSInstance.PitchValue;
+
+        AudioSampleHistory.Add(currentPitch);
+        totalDeltaTime += Time.deltaTime;
+
+        if (AudioSampleHistory.Count > AudioSampleHistoryLimit ||
+            totalDeltaTime > durationToAnalyze) {
+            AudioSampleHistory.RemoveAt(0);
+            totalDeltaTime -= Time.deltaTime;
+        }
+    }
+
+    //https://stackoverflow.com/questions/11581101/c-sharp-convert-liststring-to-dictionarystring-string
+    enum Note {
+        C,Db,D,Eb,E,F,G,A,B
+    }
+    Note AnalyzeAudioSampleHistory() {
+        //todo
+        var AudioSampleDictionary = AudioSampleHistory.ToLookup(x => x);
+        return Note.C;
+    }
 
     void MainInitialization() {
-        audioMeasureCSInstance = GetComponent<AudioMeasureCS>();
+        AudioMeasureCSInstance = GetComponent<AudioMeasureCS>();
     }
 
     #region unity functions
     // Use this for initialization
     void Start () {
-		
+        MainInitialization();
 	}
 	
 	// Update is called once per frame
@@ -29,3 +69,4 @@ public class MicAudioSimplifier : MonoBehaviour {
 
     #endregion unity functions
 }
+
